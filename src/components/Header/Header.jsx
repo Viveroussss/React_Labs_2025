614645cee3b61bdc1e320b4bcfb65ce6915be2f4
Viveroussss <dianastavickaa@gmail.com>
2025-05-08 19:50:14 +0300

    fix: remove class components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
 import { Cart } from '../Cart/Cart';
 import { LogoIcon } from '../../assets/icons/icons';
 
-
-export class Header extends Component {
-  render() {
-    const { cartCount } = this.props;
-
-    return (
-      <header className="header-container">
-        <LogoIcon className="logo-container logo-icon" />
-
-        <div className="header-content">
-          <nav className="nav-list">
-            <a href="#" className="nav-link" onClick={(e) => e.preventDefault()}>Home</a>
-            <a href="#" className="nav-link" onClick={(e) => e.preventDefault()}>Menu</a>
-            <a href="#" className="nav-link" onClick={(e) => e.preventDefault()}>Company</a>
-            <a href="#" className="nav-link" onClick={(e) => e.preventDefault()}>Login</a>
-          </nav>
-          <Cart cartCount={cartCount} />
-        </div>
-      </header>
-    );
-  }
-}
+export const Header = ({ cartCount }) => {
+  return (
+    <header className="header-container">
+      <LogoIcon className="logo-container logo-icon" />
+      <div className="header-content">
+        <nav className="nav-list">
+          {["Home", "Menu", "Company", "Login"].map((name) => (
+            <a key={name} href="#" className="nav-link" onClick={(e) => e.preventDefault()}>
+              {name}
+            </a>
+          ))}
+        </nav>
+        <Cart cartCount={cartCount} />
+      </div>
+    </header>
+  );
+};
--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -1,53 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
 import { MenuContent } from '../../components/MenuContent/MenuContent';
 
-export class MenuPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cartItems: [],
-    };
-  }
+export const MenuPage = () => {
+  const [cartItems, setCartItems] = useState([]);
 
-  addItem = (item, quantity = 1) => {
-    this.setState((prevState) => {
-      const existingItem = prevState.cartItems.find(
-        (cartItem) => cartItem.id === item.id
-      );
-
+  const addItem = (item, quantity = 1) => {
+    setCartItems((prevCartItems) => {
+      const existingItem = prevCartItems.find((cartItem) => cartItem.id === item.id);
       if (existingItem) {
-        return {
-          cartItems: prevState.cartItems.map((cartItem) =>
-            cartItem.id === item.id
-              ? { ...cartItem, quantity: cartItem.quantity + quantity }
-              : cartItem
-          ),
-        };
+        return prevCartItems.map((cartItem) =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + quantity }
+            : cartItem
+        );
       } else {
-        return {
-          cartItems: [...prevState.cartItems, { ...item, quantity }],
-        };
+        return [...prevCartItems, { ...item, quantity }];
       }
     });
   };
 
-  getTotalCount = () => {
-    const { cartItems } = this.state;
+  const getTotalCount = () => {
     return cartItems.reduce((total, item) => total + item.quantity, 0);
   };
 
-  render() {
-    const { cartItems } = this.state;
-    const cartCount = this.getTotalCount();
-
-    return (
-      <div>
-        <Header cartCount={cartCount} />
-        <MenuContent cartItems={cartItems} addItem={this.addItem} />
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header cartCount={getTotalCount()} />
+      <MenuContent addItem={addItem} />
+      <Footer />
+    </div>
+  );
+};
